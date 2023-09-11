635aec8ccc9941176a57cc1a7dcbaca91c75f121
Valentina Rojas <valenthinarojasr@gmail.com>
2023-09-11 15:58:08 -0300

    fix:jpg a webp

--- Client/src/components/atoms/image/index.jsx
+++ Client/src/components/atoms/image/index.jsx
@@ -1,5 +1,12 @@
-import { Img } from "./styled";
+import { StyledImg, StyledSrc } from "./styled";
 
 export function Image({ src }) {
-  return <Img loading={"lazy"} src={src} />;
+  let JPG = src.replace(/\.webp/g, ".jpg");
+  return (
+    <picture>
+      <StyledSrc type="image/webp" srcSet={src} />
+
+      <StyledImg loading={"lazy"} src={JPG} />
+    </picture>
+  );
 }
--- Client/src/components/atoms/image/styled.js
+++ Client/src/components/atoms/image/styled.js
@@ -1,6 +1,12 @@
 import styled, { css } from "styled-components";
 
-export const Img = styled.img`
+export const StyledImg = styled.img`
+  object-fit: cover;
+  object-position: center;
+  border-radius: 13px;
+  width: 100%;
+`;
+export const StyledSrc = styled.source`
   object-fit: cover;
   object-position: center;
   border-radius: 13px;
